cc8babfc1ba625a4ec900552e7e06870661461ba
SK SALMA <salma.23bce20344@vitapstudent.ac.in>
2026-03-08 20:21:59 +0530

    Create cache.py

---
+++ src/cache.py
@@ -0,0 +1,32 @@
+# src/cache.py
+
+class SemanticCache:
+    def __init__(self):
+        self.store = {}
+        self.hit_count = 0
+        self.miss_count = 0
+
+    def get(self, key):
+        if key in self.store:
+            self.hit_count += 1
+            return self.store[key], True
+        self.miss_count += 1
+        return None, False
+
+    def set(self, key, value):
+        self.store[key] = value
+
+    def clear(self):
+        self.store = {}
+        self.hit_count = 0
+        self.miss_count = 0
+
+    def stats(self):
+        total = len(self.store)
+        hit_rate = self.hit_count / (self.hit_count + self.miss_count) if (self.hit_count + self.miss_count) > 0 else 0
+        return {
+            "total_entries": total,
+            "hit_count": self.hit_count,
+            "miss_count": self.miss_count,
+            "hit_rate": round(hit_rate, 3)
+        }
